867055b51c58c345adf9be083b6df2cf8279542e
Fa <zalfaz749@gmail.com>
2024-10-04 10:52:41 +0700

    Add files via upload

---
+++ Algoritma 4 Okt 2024.py
@@ -0,0 +1,36 @@
+list_buah = ['Durian','Jambu','Pisang','Apel','Mangga']
+print('list_buah:', list_buah)
+list_angka=['1/2','1/3','1/10']
+print('list_angka', list_angka)
+print(list_buah[0])
+print(list_buah[-1])
+print(list_buah[-2])
+print(list_buah[-3])
+print(list_buah[-4])
+print(list_buah[0:3])
+print(list_buah[2:3])
+print(list_buah[0:1])
+print(list_buah[4:2])
+list_buah[1] = 'kesemek'
+print(list_buah)
+list_buah.append('kerakbunting')
+print(list_buah)
+list_buah.insert(0,'semangka')
+print(list_buah)
+list_buah.insert(3,'jumput cempedak')
+print(list_buah)
+list_angka.pop(0)
+'1/2'
+print(list_angka)
+list_buah.pop(5)
+'Apel'
+print(list_buah)
+list_buah.pop(0)
+'Durian'
+print(list_buah)
+list_buah.remove('kesemek')
+print(list_buah)
+del list_buah[3]
+print(list_buah)
+list_buah.sort
+print(list_buah)
